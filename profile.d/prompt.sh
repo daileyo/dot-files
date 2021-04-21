49d66506efa0561fc3362ac27bb39b1c829481fe
Robert Dailey <dale13@gmail.com>
2021-04-21 13:29:26 -0700

    fixing git prompt

--- profile.d/prompt.sh
+++ profile.d/prompt.sh
@@ -8,9 +8,10 @@ PS1="$PS1"'\u@\h '                              # user@host<space>
 PS1="$PS1"'\[\033[35m\]'                        # change to purple
 PS1="$PS1"'\[\033[33m\]'                        # change to brownish yellow
 PS1="$PS1"'\w'                                  # current working directory
+                                                #git specific
+PS1="$PS1"'\[\033[36m\]'                        # change color to cyan
+PS1="$PS1"'`__git_ps1`'                         # bash function
+                                                #end git specific
 PS1="$PS1"'\[\033[0m\]'                         # change color
 PS1="$PS1"'\n'                                  # new line
 PS1="$PS1"'$ '                                  # prompt: always $
-#git specific
-PS1="$PS1"'\[\033[36m\]'  # change color to cyan
-PS1="$PS1"'`__git_ps1`'   # bash function
